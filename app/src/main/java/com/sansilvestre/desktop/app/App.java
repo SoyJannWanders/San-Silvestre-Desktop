8a450dbfb3804152b1d6fd792c1b8a73aa28a2e8
SoyJannWanders <soyjannwanders@gmail.com>
2024-03-11 21:42:19 -0300

    🍃 Development Initialization

--- app/src/main/java/com/sansilvestre/desktop/app/App.java
+++ app/src/main/java/com/sansilvestre/desktop/app/App.java
@@ -4,13 +4,41 @@
 
 package com.sansilvestre.desktop.app;
 
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+
+import javax.swing.*;
+import java.awt.*;
+
 /**
  *
  * @author Jan
  */
-public class App {
+public class App extends JFrame {
+
+    private final short MIN_WIDTH = 1200;
+    private final short MIN_HEIGHT = 700;
+
+    private static final String FLATLAF_PROPIERTIES_FOLDER = "themes";
+
+    public App() {
+        setTitle("San Silvestre");
+        setSize(new Dimension(MIN_WIDTH, MIN_HEIGHT));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        initFlatlaf();
+        EventQueue.invokeLater(() -> new App().setVisible(true));
     }
+
+    private static void initFlatlaf() {
+        FlatRobotoFont.install();
+        FlatLaf.registerCustomDefaultsSource(FLATLAF_PROPIERTIES_FOLDER);
+        UIManager.put("defaultFont", new Font(FlatRobotoFont.FAMILY, Font.PLAIN, 14));
+        FlatMacLightLaf.setup();
+    }
+
 }
