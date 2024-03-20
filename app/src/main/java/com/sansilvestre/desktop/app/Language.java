6c8cae5dbc4802415b190003f2b3a1da1b200191
SoyJannWanders <soyjannwanders@gmail.com>
2024-03-20 12:42:46 -0300

    🌎 Adding multiple language functionality

---
+++ app/src/main/java/com/sansilvestre/desktop/app/Language.java
@@ -0,0 +1,13 @@
+package com.sansilvestre.desktop.app;
+
+import java.util.ResourceBundle;
+
+public class Language {
+
+    private static final String SYSTEM_LANGUAGE = "strings/es/"; // Spanish
+
+    public static String getSystemLanguage() {
+        return SYSTEM_LANGUAGE;
+    }
+
+}
