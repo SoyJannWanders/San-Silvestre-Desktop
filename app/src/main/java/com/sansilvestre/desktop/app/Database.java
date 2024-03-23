74bec29e3deec9b7189046af3cb861b2e6169100
Jann Wanders <soyjannwanders@gmail.com>
2024-03-23 01:08:46 -0300

    📂 Database connection class (Only for testing)

---
+++ app/src/main/java/com/sansilvestre/desktop/app/Database.java
@@ -0,0 +1,9 @@
+package com.sansilvestre.desktop.app;
+
+public class Database {
+
+    public static final String LOCAL_URL = "jdbc:mysql://localhost:3306/silvestry";
+    public static final String LOCAL_USER = "root";
+    public static final String LOCAL_PASSWORD = "Jann.wanders";
+
+}
